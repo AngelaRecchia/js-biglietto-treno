4c84b3dcf7050a20b6eeae0f4091c2ff1d4248d1
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-05-27 15:13:36 +0200

    add print sconto

--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@ var km = parseFloat(prompt("Quanti km devi percorrere?"));
 var eta = parseInt(prompt("Quanti anni hai?"));
 
 var prezzo = km * 0.21;
+var minorenne = eta < 18;
+var over65 = eta >= 65;
 
-if (eta < 18) {
-    prezzo *= 0.8;
-} else if (eta > 65) {
-    prezzo *= 0.6;
-} 
+if (minorenne || over65) {
+    document.getElementById('text').innerHTML = "Hai diritto ad uno sconto del ";
 
-document.getElementById('text').innerHTML = "Il biglietto costa: " + prezzo.toFixed(2) + "€.";
+    if (minorenne) {
+        prezzo *= 0.8;
+        document.getElementById('text').innerHTML += "20% </br>";
+
+    } else if (over65) {
+        prezzo *= 0.6;
+        document.getElementById('text').innerHTML += "40% </br>";
+    } 
+
+}
+
+document.getElementById('text').innerHTML += "Il biglietto costa: " + prezzo.toFixed(2) + "€";
